7458fb1ca21cb853ad5be52b4b84aa0909af95a8
신충용 <cystudy93@gmail.com>
2025-01-12 23:30:20 +0900

    restcontroller로 수정

--- src/main/java/com/example/portfolio/controller/HomeController.java
+++ src/main/java/com/example/portfolio/controller/HomeController.java
@@ -2,16 +2,21 @@ package com.example.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
-@Controller
+@RestController
 public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
+	  @GetMapping("/env")
+	    public String env() {
+	        return "Environment info here!";
+	    }
 	
 	
 }
